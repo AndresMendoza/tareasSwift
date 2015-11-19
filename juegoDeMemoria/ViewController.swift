6a4793d9ecd62a08b279924b6616eca075073605
Andrés Francisco Guatzozón Mendoza <andrew0992@gmail.com>
2015-11-18 23:18:10 -0600

    Initial Commit

---
+++ juegoDeMemoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  juegoDeMemoria
+//
+//  Created by Andrés Francisco Guatzozón Mendoza on 11/18/15.
+//  Copyright © 2015 Andrés Francisco Guatzozón Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
